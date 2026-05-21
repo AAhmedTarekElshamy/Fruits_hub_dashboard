6000f3c923394e88de579416b4f613c2983984a6
Ahmed Tarik <ahmedtarik20188@gmail.com>
2026-05-21 23:10:25 +0300

    initial Flutter project setup

---
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import '../../features/dashboard/views/dashboard_view.dart';
+
+
+Route<dynamic> onGenerateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case DashboardView.routeName:
+      return MaterialPageRoute(builder: (context) => const DashboardView());
+
+    default:
+      return MaterialPageRoute(
+        builder: (context) => const Scaffold(
+          body: Center(
+            child: Text('Route not found'),
+          ),
+        ),
+      );
+  }
+}
