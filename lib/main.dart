fec4791808452588e1633480e159bf140a66296f
Ahmed Tarik <ahmedtarik20188@gmail.com>
2026-05-21 23:21:36 +0300

    add Firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fruits_hub_dashboard/core/helper_functions/on_generate_routes.dart';
-
 import 'features/dashboard/views/dashboard_view.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-       onGenerateRoute: onGenerateRoute,
+      onGenerateRoute: onGenerateRoute,
       initialRoute: DashboardView.routeName,
-      //home: const DashboardView( ),
     );
   }
 }
-
